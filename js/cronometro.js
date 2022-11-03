79ef079520959da2d6f2ebb5685287e9bb023b72
Miguel Roa <miguel_roa150@outlook.es>
2022-11-03 12:23:28 -0500

    juego arreglado

---
+++ js/cronometro.js
@@ -0,0 +1,33 @@
+//* Nicolas Naranjo
+
+function iniciaCronometro() {
+  var segundos = 60;
+  var minutos = 0;
+  var segundosTexto;
+  var minutosTexto;
+  //Determinar el tiempo en segundo para poder tener un mejor contador recuerda no pasar de 60segundos
+  function actualizaContador() {
+    segundos--;
+    if (segundos < 0) {
+      segundos = 60;
+      minutos--;
+    }
+    if (minutos < 0) {
+      segundos = 0;
+      minutos = 0;
+      clearInterval(cronometro);
+      timeOver();
+    }
+    segundosTexto = segundos;
+    minutosTexto = minutos;
+    if (segundos < 60) {
+      segundosTexto = "0" + segundos;
+    }
+    if (minutos < 10) {
+      minutosTexto = "0" + minutos;
+    }
+    document.querySelector("#minutos").innerText = minutosTexto;
+    document.querySelector("#segundos").innerText = segundosTexto;
+  }
+  cronometro = setInterval(actualizaContador, 1000);
+}
